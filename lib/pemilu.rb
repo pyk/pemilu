261cae8e8136ecf7879ac013cc8662a0a35a9b99
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-01 23:51:01 +0700

    load all class into module

--- lib/pemilu.rb
+++ lib/pemilu.rb
@@ -1,6 +1,9 @@
 require "net/http"
 require "json"
 require "pemilu/version"
+require "pemilu/api"
+require "pemilu/candidate"
+require "pemilu/hash_extension"
 
 module Pemilu
   # Your code goes here...
