20bd0c2c1a4a626db1a5c362a0a94dad877c1c53
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-02 07:06:01 +0700

    add #parties and #party(id) spec

--- spec/pemilu/api_spec.rb
+++ spec/pemilu/api_spec.rb
@@ -68,5 +68,31 @@ module Pemilu
       end
     end
 
+    describe "#parties" do
+      let(:pemilu) { Pemilu::API.new(key: "06ec082d057daa3d310b27483cc3962e") }
+
+      it "should return an array of Pemilu::Party object" do
+        pemilu.parties.should be_a(Array)
+        pemilu.parties.each do |party|
+          expect(party.class).to eq(Pemilu::Party)
+        end
+      end
+    end
+
+    describe "#party(id)" do
+      let(:pemilu) { Pemilu::API.new(key: "06ec082d057daa3d310b27483cc3962e") }
+
+      it "should return Pemilu::Party object and have correct details" do
+        party = pemilu.party(1)
+        party.should be_a(Pemilu::Party)
+        expect(party.id).to eq(1)
+        expect(party.nick_name).to eq("Partai NasDem")
+        expect(party.full_name).to eq("Partai NasDem")
+        expect(party.url).to eq("http://www.partainasdem.org/")
+        expect(party.facebook).to eq("https://www.facebook.com/pages/Partai-Nasdem/135724689838285")
+        expect(party.twitter).to eq("https://twitter.com/NasDem")
+      end
+    end
+
   end
 end
