bb3ed1f9ee2bb73ec796e80bc138bfd372c5ff2d
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-02 09:28:39 +0700

    add province spec

---
+++ spec/pemilu/province_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+module Pemilu
+  describe Province do
+    describe "new" do
+      let(:province) { Pemilu::Province.new(
+        id: 1,
+        name: "Sidoarjo",
+        full_name: "Sidoarjo",
+        international_name: "Sidoarjo jos!",
+        available_sheets: 1,
+        population: 100000
+      ) }
+
+      it "should an object of Pemilu::Province and return correct details" do
+        expect(province.class).to eq(Pemilu::Province)
+        expect(province.id).to eq(1)
+        expect(province.name).to eq("Sidoarjo")
+        expect(province.full_name).to eq("Sidoarjo")
+        expect(province.international_name).to eq("Sidoarjo jos!")
+        expect(province.available_sheets).to eq(1)
+        expect(province.population).to eq(100000)
+      end
+    end
+  end
+end
