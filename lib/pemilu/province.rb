fb9beb796dfa8bc15e1777a2228dceda57aeaf01
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-02 10:16:47 +0700

    add another attribute: electoral_district

--- lib/pemilu/province.rb
+++ lib/pemilu/province.rb
@@ -1,15 +1,16 @@
 module Pemilu
   class Province
 
-    attr_reader :id, :name, :full_name, :international_name, :available_chairs, :population
+    attr_reader :id, :name, :full_name, :international_name, :available_chairs, :population, :electoral_district
 
-    def initialize(id: nil, name: nil, full_name: nil, international_name: nil, available_chairs: nil, population: nil)
+    def initialize(id: nil, name: nil, full_name: nil, international_name: nil, available_chairs: nil, population: nil, electoral_district: nil)
       @id = id
       @name = name
       @full_name = full_name
       @international_name = international_name
       @available_chairs = available_chairs
       @population = population
+      @electoral_district = electoral_district
     end
 
   end
