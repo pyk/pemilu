309e1a253bb648463063ac84791b33a08919e665
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-02 12:23:12 +0700

    initialize Pemilu::ElectoralDistrict spec

---
+++ spec/pemilu/electoral_district_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+module Pemilu
+  describe ElectoralDistrict do
+    describe "new" do
+      let(:ed) { Pemilu::ElectoralDistrict.new(
+        id: 1,
+        full_name: "test",
+        legislative_body: "test",
+        available_chairs: 1,
+        population: 1,
+        province: {
+        "id" => 1,
+        "name" => "test"
+      })}
+
+      it "should become Pemilu::ElectoralDistrict object and have correct details" do
+        ed.should be_a(Pemilu::ElectoralDistrict)
+        expect(ed.id).to eq(1)
+        expect(ed.full_name).to eq("test")
+        expect(ed.legislative_body).to eq("test")
+        expect(ed.available_chairs).to eq(1)
+        expect(ed.population).to eq(1)
+        ed.province.should be_a(Hash)
+        expect(ed.province.id).to eq(1)
+        expect(ed.province.name).to eq("test")
+      end
+    end
+  end
+end
