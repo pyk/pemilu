b4f080fdcc84d92748b6a9672591f761dcc3ec99
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-02 12:27:47 +0700

    add spec for #electoral_districts and #electoral_district(id)

--- spec/pemilu/api_spec.rb
+++ spec/pemilu/api_spec.rb
@@ -117,5 +117,33 @@ module Pemilu
       end
     end
 
+    describe "#electoral_districts" do
+      let(:pemilu) { Pemilu::API.new(key: "06ec082d057daa3d310b27483cc3962e") }
+
+      it "should return an array of Pemilu::ElectoralDistrict object" do
+        pemilu.electoral_districts.should be_a(Array)
+        pemilu.electoral_districts.each do |ed|
+          expect(ed.class).to eq(Pemilu::ElectoralDistrict)
+        end
+      end
+    end
+
+    describe "#electoral_district(id)" do
+      let(:pemilu) { Pemilu::API.new(key: "06ec082d057daa3d310b27483cc3962e") }
+
+      it "should return Pemilu::ElectoralDistrict object and have correct details" do
+        electoral_district = pemilu.electoral_district("1101-00-0000")
+        electoral_district.should be_a(Pemilu::ElectoralDistrict)
+        expect(electoral_district.id).to eq("1101-00-0000")
+        expect(electoral_district.name).to eq("Aceh I")
+        expect(electoral_district.legislative_body).to eq("DPR")
+        expect(electoral_district.available_chairs).to eq(7)
+        expect(electoral_district.population).to eq(2642760)
+        electoral_district.province.should be_a(Hash)
+        expect(electoral_district.province.id).to eq(11)
+        expect(electoral_district.province.name).to eq("Aceh")
+      end
+    end
+
   end
 end
