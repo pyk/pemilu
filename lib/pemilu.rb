3b8250105bbc73812d4c355638384b5455f92adb
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-03-02 09:45:35 +0700

    implement #provinces and #province(id)

--- lib/pemilu.rb
+++ lib/pemilu.rb
@@ -5,6 +5,7 @@ require "pemilu/api"
 require "pemilu/candidate"
 require "pemilu/hash_extension"
 require "pemilu/party"
+require "pemilu/province"
 
 module Pemilu
   # Your code goes here...
--- lib/pemilu/api.rb
+++ lib/pemilu/api.rb
@@ -146,5 +146,44 @@ module Pemilu
         )
     end
 
+    def provinces
+      uri = URI("http://api.pemiluapi.org/candidate/api/provinsi")
+      params = { apiKey: @key }
+      uri.query = URI.encode_www_form(params)
+      respond = Net::HTTP.get_response(uri)
+      result = []
+
+      data = JSON.parse(respond.body) if respond.is_a?(Net::HTTPSuccess)
+      @total_provinsi = data["data"]["results"]["count"]
+      provinces = data["data"]["results"]["provinsi"]
+      provinces.each do |province|
+        result << Pemilu::Province.new(
+          id: province["id"].to_i,
+          name: province["nama"],
+          full_name: province["nama_lengkap"],
+          international_name: province["nama_inggris"],
+          available_chairs: province["jumlah_kursi"],
+          population: province["jumlah_penduduk"])
+      end
+      return result
+    end
+
+    def province(id)
+      uri = URI("http://api.pemiluapi.org/candidate/api/provinsi/#{id}")
+      params = { apiKey: @key }
+      uri.query = URI.encode_www_form(params)
+      respond = Net::HTTP.get_response(uri)
+
+      data = JSON.parse(respond.body) if respond.is_a?(Net::HTTPSuccess)
+      province = data["data"]["results"]["provinsi"][0]
+      return Pemilu::Province.new(
+          id: province["id"].to_i,
+          name: province["nama"],
+          full_name: province["nama_lengkap"],
+          international_name: province["nama_inggris"],
+          available_chairs: province["jumlah_kursi"],
+          population: province["jumlah_penduduk"])
+      end
+
   end
 end
